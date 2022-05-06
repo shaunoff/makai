4c6ab986a751243b59ee4c65746389a88043a0f6
shaun Hutch <shaun@flipsidecrypto.com>
2022-05-06 10:15:16 -0400

    video test (#1)

---
+++ app/components/Header.tsx
@@ -0,0 +1,57 @@
+import React from "react";
+
+import MobileMenu from "./MobileMenu";
+
+const navigation = [
+  { name: "Services", href: "#" },
+  { name: "8(a) Contracting", href: "#" },
+  { name: "Kekoa Foundation", href: "#" },
+  { name: "Contact Us", href: "#" },
+];
+
+const Header = () => {
+  return (
+    <div className="bg-gray-900 pt-6">
+      <nav
+        className="relative mx-auto flex max-w-7xl items-center justify-between px-4 sm:px-6"
+        aria-label="Global"
+      >
+        <div className="flex flex-1 items-center">
+          <div className="flex w-full items-center justify-between md:w-auto">
+            <a href="/test">
+              <span className="sr-only">Workflow</span>
+              <img
+                className="h-20 w-auto brightness-0 invert filter sm:h-20"
+                src="images/Makai.png"
+                alt=""
+              />
+            </a>
+
+            <MobileMenu navigation={navigation} />
+          </div>
+          <div className="hidden space-x-8 md:ml-10 md:flex">
+            {navigation.map((item) => (
+              <a
+                key={item.name}
+                href={item.href}
+                className="text-base font-medium text-white hover:text-gray-300"
+              >
+                {item.name}
+              </a>
+            ))}
+          </div>
+        </div>
+        <div className="hidden md:flex md:items-center md:space-x-6">
+          <a
+            href="/test"
+            className="text-base font-medium text-white hover:text-gray-300"
+          >
+            Log in
+          </a>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Header;
---
+++ app/components/MobileMenu.tsx
@@ -0,0 +1,83 @@
+import { Fragment } from "react";
+import { Popover, Transition } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+
+interface MobileMenuProps {
+  navigation: Record<string, string>[];
+}
+
+const MobileMenu = ({ navigation }: MobileMenuProps) => {
+  return (
+    <>
+      <Popover as="div" className="-mr-2 flex items-center md:hidden">
+        <Popover.Button className="focus-ring-inset inline-flex items-center justify-center rounded-md bg-gray-900 p-2 text-gray-400 hover:bg-gray-800 focus:outline-none focus:ring-2 focus:ring-white">
+          <span className="sr-only">Open main menu</span>
+          <MenuIcon className="h-6 w-6" aria-hidden="true" />
+        </Popover.Button>
+        <Transition
+          as={Fragment}
+          enter="duration-150 ease-out"
+          enterFrom="opacity-0 scale-95"
+          enterTo="opacity-100 scale-100"
+          leave="duration-100 ease-in"
+          leaveFrom="opacity-100 scale-100"
+          leaveTo="opacity-0 scale-95"
+        >
+          <Popover.Panel
+            focus
+            className="absolute inset-x-0 top-0 origin-top transform p-2 transition md:hidden"
+          >
+            <div className="overflow-hidden rounded-lg bg-white shadow-md ring-1 ring-black ring-opacity-5">
+              <div className="flex items-center justify-between px-5 pt-4">
+                <div>
+                  <img
+                    className="h-8 w-auto"
+                    src="https://tailwindui.com/img/logos/workflow-mark-teal-500-cyan-600.svg"
+                    alt=""
+                  />
+                </div>
+                <div className="-mr-2">
+                  <Popover.Button className="inline-flex items-center justify-center rounded-md bg-white p-2 text-gray-400 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-cyan-600">
+                    <span className="sr-only">Close menu</span>
+                    <XIcon className="h-6 w-6" aria-hidden="true" />
+                  </Popover.Button>
+                </div>
+              </div>
+              <div className="pt-5 pb-6">
+                <div className="space-y-1 px-2">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      className="block rounded-md px-3 py-2 text-base font-medium text-gray-900 hover:bg-gray-50"
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
+                <div className="mt-6 px-5">
+                  <a
+                    href="/test"
+                    className="block w-full rounded-md bg-gradient-to-r from-teal-500 to-cyan-600 py-3 px-4 text-center font-medium text-white shadow hover:from-teal-600 hover:to-cyan-700"
+                  >
+                    Start free trial
+                  </a>
+                </div>
+                <div className="mt-6 px-5">
+                  <p className="text-center text-base font-medium text-gray-500">
+                    Existing customer?{" "}
+                    <a href="/test" className="text-gray-900 hover:underline">
+                      Login
+                    </a>
+                  </p>
+                </div>
+              </div>
+            </div>
+          </Popover.Panel>
+        </Transition>
+      </Popover>
+    </>
+  );
+};
+
+export default MobileMenu;
